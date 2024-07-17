0dcb492c4f931a327374fb162bd3df1871135d8e
Pandemo77 <arno.maat@gmail.com>
2024-07-17 15:56:38 +0200

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using API.Data;
+﻿using System.Security.Claims;
+using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
@@ -10,7 +12,7 @@ using Microsoft.VisualBasic;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseAPIController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseAPIController
 {
  
   [HttpGet]
@@ -30,6 +32,22 @@ public class UsersController(IUserRepository userRepository) : BaseAPIController
     if (user == null) return NotFound();
 
     return user;
+  }
 
+  [HttpPut]
+  public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) {
+    var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+      if(username == null) return BadRequest("No username found in token");
+
+      var user = await userRepository.GetUserByUsernameAsync(username);
+
+      if(user == null) return BadRequest("Nothing goes well is it! (not finding any users!!)");
+
+      mapper.Map(memberUpdateDto, user);
+
+      if(await userRepository.SaveAllAsync()) return NoContent();
+
+      return BadRequest("Failed to update the user");
   }
 }
